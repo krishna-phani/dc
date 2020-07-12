dc44c30b45b854c9060a478ca9e37859b7203a2a
krishna <krishna.phani@keyvertex.com>
2020-07-12 21:10:39 +0530

    Updates

---
+++ precache-manifest.e76a6774c967879d50da574bd13e12a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b507065cafacce0e48fc63588bbad21f",
+    "url": "/dc/index.html"
+  },
+  {
+    "revision": "16f0bcc26d26550764c1",
+    "url": "/dc/static/css/main.57d161a2.chunk.css"
+  },
+  {
+    "revision": "1fb1f775a8b66952e654",
+    "url": "/dc/static/js/2.2b48fb0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dc/static/js/2.2b48fb0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f0bcc26d26550764c1",
+    "url": "/dc/static/js/main.03ab5e28.chunk.js"
+  },
+  {
+    "revision": "a3f56cabf0a3cea09f13",
+    "url": "/dc/static/js/runtime-main.c64c3243.js"
+  }
+]);
